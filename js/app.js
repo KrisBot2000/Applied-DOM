9c05efb93557a3e5d86dad92fc0df6e69c38c3a6
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-22 18:48:47 -1000

    add button 9

--- js/app.js
+++ js/app.js
@@ -168,8 +168,8 @@
    * to set the context to the correct object
    *   and passing two aditional arguments, event and 'light-green'
    */
-   btn9.addEventListener("click", function(){
-
+   btn9.addEventListener("click", event => {
+    setMyLightClass.call(btn9, event, 'light-green');
    });
 
   /*
